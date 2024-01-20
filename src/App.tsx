806f3a1d5b75858c33654bbce57d2f2671611400
Anatoli Gavrash <gaver5@yandex.ru>
2024-01-20 23:59:17 +0300

    Add vite base configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="header"></header>
+      <main className="main"></main>
+    </div>
+  )
+};
+
+export default App;
