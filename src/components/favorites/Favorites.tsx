5f980870305aefa8d7e9061f6f2fa634bb2eea36
Anatoli Gavrash <gaver5@yandex.ru>
2024-01-29 03:11:40 +0300

    Add purchases page logic

--- src/components/favorites/Favorites.tsx
+++ src/components/favorites/Favorites.tsx
@@ -1,66 +1,22 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getComicsFromIdList, libraryData, resetLibrary } from '../../store/slices/library/library-slice';
-import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
-import type { Comic } from '../../store/slices/library/library.types';
-import { searchFilter, validationPageValue } from '../../utils/utils';
-import styles from './Favorites.module.scss';
-import Pagination from '../pagination';
-import ComicList from '../comic-list';
-import Search from '../search';
+import { useAppSelector } from '../../hooks/hooks';
 import { currentUser } from '../../store/slices/login/login-slice';
+import styles from './Favorites.module.scss';
+import LocalLibrary from '../local-library';
 
 const Favorites: React.FC = () => {
-  const dispatch = useAppDispatch();
-  const user = useAppSelector(currentUser);
-  const libData = useAppSelector(libraryData);
   const {idFavorite} = useParams();
-  const [offset, setOffset] = useState<number>(0);
-  const [cardCountOnPage, setCardCountOnPage] = useState<number>(20);
-  const [maxPages, setMaxPages] = useState<number>(1);
-  const [comicsList, setComicsList] = useState<Comic[] | null>(null);
-  const [searchValue, setSearchValue] = useState<string>('');
-  const defaultPath = '/favorites/';
-  const currentPage = validationPageValue(1, maxPages, Number(idFavorite));
-
-  useEffect(() => {
-    if (user?.favorites) {
-      const ids = searchFilter(searchValue, user.favorites);
-      
-      dispatch(getComicsFromIdList({
-        idList: ids,
-        customParams: {
-          limit: cardCountOnPage,
-          offset: offset
-        }
-      }))
-    };
-    
-    return () => {
-      dispatch(resetLibrary());
-    };
-  }, [dispatch, user?.favorites, offset, cardCountOnPage, searchValue]);
-
-  useEffect(() => {
-    const newOffset = cardCountOnPage * (currentPage - 1);
-    if (offset !== newOffset) setOffset(newOffset);
-  }, [offset, cardCountOnPage, currentPage]);
-
-  useEffect(() => {
-    if (libData?.results) setComicsList(libData.results);
-    if (libData?.total && libData?.limit) setMaxPages(Math.ceil(libData.total / libData.limit));
-  }, [libData]);
+  const user = useAppSelector(currentUser);
 
   return (
     <section className={styles.favorites}>
       <h2 className={styles.title}>Избранное</h2>
       <hr className={styles.delimeter} />
-      {libData && currentPage && <>
-        <Pagination path={defaultPath} currentPage={currentPage} maxPages={maxPages} />
-        <Search setSearch={setSearchValue} />
-        <ComicList cardList={comicsList} />
-        <Pagination path={defaultPath} currentPage={currentPage} maxPages={maxPages} />
-      </>}
+      <LocalLibrary
+        pageId={idFavorite || ''}
+        pagePath={'/favorites/'}
+        userData={user?.favorites}
+      />
     </section>
   );
 };
--- src/components/purchases/Purchases.tsx
+++ src/components/purchases/Purchases.tsx
@@ -1,10 +1,22 @@
+import { useParams } from 'react-router-dom';
+import { useAppSelector } from '../../hooks/hooks';
+import { currentUser } from '../../store/slices/login/login-slice';
 import styles from './Purchases.module.scss';
+import LocalLibrary from '../local-library';
 
 const Purchases: React.FC = () => {
+  const {idPurchase} = useParams();
+  const user = useAppSelector(currentUser);
+
   return (
     <section className={styles.purchases}>
       <h2 className={styles.title}>Покупки</h2>
       <hr className={styles.delimeter} />
+      <LocalLibrary
+        pageId={idPurchase || ''}
+        pagePath={'/purchases/'}
+        userData={user?.purchases}
+      />
     </section>
   );
 };
