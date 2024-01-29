4fa537e0e6af7270924d5e8c6ae11a12ff0c363b
Anatoli Gavrash <gaver5@yandex.ru>
2024-01-30 01:09:50 +0300

    Change nav component

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,25 +1,34 @@
-import { Favorite, ShoppingCart } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
-import styles from './Nav.module.scss';
-import Authorize from '../authorize';
+import { Tooltip } from '@mui/material';
+import { Favorite, ShoppingCart } from '@mui/icons-material';
 import { useAppSelector } from '../../hooks/hooks';
 import { currentUser } from '../../store/slices/login/login-slice';
+import styles from './Nav.module.scss';
+import Authorize from '../authorize';
 
 const Nav: React.FC = () => {
   const user = useAppSelector(currentUser);
 
   return (
-    <div className={styles.nav}>
-      {user && <>
-        <Link className={styles.link} to={'/favorites/'}>
-          <Favorite fontSize={'large'} />
-        </Link>
-        <Link className={styles.link} to={'/purchases/'}>
-          <ShoppingCart fontSize={'large'} />
-        </Link>
-      </>}
+    <nav className={styles.nav}>
+      <ul className={styles.list}>
+        {user && <li className={styles.item}>
+          <Tooltip title={'Избранное'} placement='bottom'>
+            <Link className={styles.link} to={'/favorites/'}>
+              <Favorite fontSize={'large'} />
+            </Link>
+          </Tooltip>
+        </li>}
+        {user && <li className={styles.item}>
+          <Tooltip title={'Покупки'} placement='bottom'>
+            <Link className={styles.link} to={'/purchases/'}>
+              <ShoppingCart fontSize={'large'} />
+            </Link>
+          </Tooltip>
+        </li>}
+      </ul>
       <Authorize />
-    </div>
+    </nav>
   );
 };
 
