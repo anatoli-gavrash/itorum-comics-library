3dbfa1f6ca0ac31dd8baf103b5df5724d5abe0a8
Anatoli Gavrash <gaver5@yandex.ru>
2024-01-31 05:47:00 +0300

    Add check is empty object function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -16,13 +16,19 @@ export const searchFilter = (string: string, data: UserStorage[]): number[] => {
   return data.filter(({title}) => title.includes(string)).map(({id}) => id);
 };
 
-export const fieldValidation = (value: string): boolean => {
-  return false;
-};
-
 export const randomInteger = (min: number = 0, max: number = 1000000000): number => {
   min = Math.ceil(min);
   max = Math.floor(max);
 
   return Math.floor(Math.random() * (max - min) + min);
 }
+
+export const isObjectEmpty = (object: Object): boolean => {
+  for (const key in object) {
+    if (Object.hasOwn(object, key)) {
+      return false;
+    }
+  }
+
+  return true;
+};
